6f9e6e974ccb996df3a0ab7d2301e82264481984
David Sulpy <david@sulpy.com>
2014-08-11 23:27:06 -0500

    fixed inline if statement syntax

--- CloudDeploy/EndpointHealthchecker.rb
+++ CloudDeploy/EndpointHealthchecker.rb
@@ -21,7 +21,7 @@ module CloudDeploy
 		end
 
 		def check_health(full_endpoint)
-			if @notify_teamcity puts "##teamcity[blockOpened name='healthchecking']" end
+			if @notify_teamcity; puts "##teamcity[blockOpened name='healthchecking']" end
 
 			puts "Starting health check"
 
@@ -30,7 +30,7 @@ module CloudDeploy
 			while (health_attempts > 0)
 				url = full_endpoint.gsub("https://", "http://")
 
-				if @debug puts "    DEBUG: checking #{url}" end
+				if @debug; puts "    DEBUG: checking #{url}" end
 
 				begin
 					url = URI.parse(url)
@@ -42,19 +42,19 @@ module CloudDeploy
 					end
 				rescue Exception => ex
 					if res != nil
-						if @debug puts "    DEBUG: code #{res.code}" end
+						if @debug; puts "    DEBUG: code #{res.code}" end
 					end
-					if @debug puts "    DEBUG: #{ex.message} (#{url})" end
+					if @debug; puts "    DEBUG: #{ex.message} (#{url})" end
 				end
 				puts "  still checking #{url} health (#{health_attempts} more attempts left, sleeping #{sleep_time} seconds)"
 				sleep sleep_time
 
 				if @sleep_calc == "linear" sleep_time = sleep_time * 2 end
 
-				if sleep_time > 120 sleep_time = 120 end 
+				if sleep_time > 120; sleep_time = 120 end 
 				health_attempts = health_attempts - 1
 			end
-			if @notify_teamcity puts "##teamcity[blockClosed name='healthchecking']" end
+			if @notify_teamcity; puts "##teamcity[blockClosed name='healthchecking']" end
 		end
 	end
 end
