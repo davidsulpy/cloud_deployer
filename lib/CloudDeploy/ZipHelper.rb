d469197c835b357659b09bbd44c1e2029786db1c
David Sulpy <david@sulpy.com>
2015-08-27 15:16:07 -0500

    changed the initialization of zip helper; added ignore capabilities to archiving

--- lib/CloudDeploy/ZipHelper.rb
+++ lib/CloudDeploy/ZipHelper.rb
@@ -21,14 +21,21 @@ module CloudDeploy
   class ZipFileGenerator
 
     # Initialize with the directory to zip and the location of the output archive.
-    def initialize(inputDir, outputFile)
-      @inputDir = inputDir
-      @outputFile = outputFile
+    def initialize(config)
+      @inputDir = config[:input_dir]
+      @outputFile = config[:output_file]
+      @patterns_to_ignore = config[:ignore]
+      @verbose = config[:verbose]
     end
 
     # Zip the input directory.
     def write()
-      entries = Dir.entries(@inputDir); entries.delete("."); entries.delete("..")
+      entries = Dir.entries(@inputDir)
+      entries.delete(".")
+      entries.delete("..")
+      @patterns_to_ignore.each do |pattern|
+        entries.delete(pattern)
+      end
       io = Zip::File.open(@outputFile, Zip::File::CREATE);
 
       writeEntries(entries, "", io)
@@ -42,13 +49,23 @@ module CloudDeploy
       entries.each { |e|
         zipFilePath = path == "" ? e : File.join(path, e)
         diskFilePath = File.join(@inputDir, zipFilePath)
-        puts "Deflating " + diskFilePath
+        if (@verbose)
+          puts "Deflating " + diskFilePath
+        end
         if  File.directory?(diskFilePath)
           io.mkdir(zipFilePath)
           subdir =Dir.entries(diskFilePath); subdir.delete("."); subdir.delete("..")
           writeEntries(subdir, zipFilePath, io)
         else
-          io.get_output_stream(zipFilePath) { |f| f.puts(File.open(diskFilePath, "rb").read())}
+          ignore = false
+          @patterns_to_ignore.each do |pattern|
+            if (File.fnmatch(pattern, e))
+              ignore = true
+            end
+          end
+          if (ignore == false)
+            io.get_output_stream(zipFilePath) { |f| f.puts(File.open(diskFilePath, "rb").read())}
+          end
         end
       }
     end
